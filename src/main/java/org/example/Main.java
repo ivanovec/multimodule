a8046a98d225e8be086b0eb9262e85a22deac373
Ivan Efimov <Ivan.Efimov@jetbrains.com>
2025-02-17 21:36:34 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "Hello, World!";
+        System.out.println("Hello, World!");
+    }
+}
